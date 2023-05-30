2ca1516b8474c718db7b64840fcac383c3a1db6b
James Gilbert <jgrg@sanger.ac.uk>
2023-05-30 13:28:11 +0100

    Script to run alembic autogenerate

---
+++ scripts/alembic_autogenerate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ $# -eq 1 ]
+then
+	msg=$1
+else
+	echo "Usage: alembic_autogenerate.sh <MESSAGE>"
+	exit 2
+fi
+
+export DB_URI="postgresql://sts-dev:build-that-sts@127.0.0.1:5435/tolqc"
+cd ~/git/tolqc/tolqc-api
+alembic -c migrations/alembic.ini revision --autogenerate -m "$msg"
