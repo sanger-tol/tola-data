96f5af28556cf140d3c30e497eabc74f78cd4044
James Gilbert <jgrg@sanger.ac.uk>
2023-07-14 14:41:21 +0100

    Switch to using marshals

--- src/tola/projects_from_tol_track_conf.py
+++ src/tola/projects_from_tol_track_conf.py
@@ -2,86 +2,43 @@
 #
 # SPDX-License-Identifier: MIT
 
-import os
 import re
-import sys
 import pathlib
+import tola.marshals
 
 from main.model import Project
-from sqlalchemy import select
-from tol.api_client import ApiDataSource, ApiObject
-from tola import db_connection
 
-
-def main(conf_file="tol_track.conf"):
-    proj_iter = conf_file_Projects(pathlib.Path(conf_file))
-    if True:
-        store_Projects_via_sql_alchemy(proj_iter)
-    else:
-        store_Projects_via_tol_api(proj_iter)
-
-
-def store_Projects_via_sql_alchemy(proj_iter):
-    engine, Session = db_connection.tola_db_engine(echo=True)
-
-    with Session() as ssn:
-        for proj in proj_iter:
-            query = select(Project).filter_by(lims_id=proj.lims_id)
-            if db_proj := ssn.scalars(query).one_or_none():
-                proj.project_id = db_proj.project_id
-                ssn.merge(proj)
-            else:
-                ssn.add(proj)
-
-        # ssn.flush()
-        ssn.commit()
-
-
-def store_Projects_via_tol_api(proj_iter):
-    tolqc = ApiDataSource(
-        {
-            'url': os.getenv('TOLQC_URL') + '/api/v1',
-            'key': os.getenv('TOLQC_API_KEY'),
-        }
+def main():
+    mrshl, file_list = tola.marshals.marshal_from_command_line(
+        "Update list of Projects from tol_track.conf (or similar) file"
     )
-    stored_projects = {p.lims_id: p for p in tolqc.get_list('projects')}
-    for proj in proj_iter:
-        print(f"Processing project with lims_id = {proj.lims_id}")
-        if db_proj := stored_projects.get(proj.lims_id):
-            db_proj.hierarchy_name = proj.hierarchy_name
-            db_proj.description = proj.description
-            tolqc.update(db_proj)
-        else:
-            api_proj = ApiObject(
-                'projects',
-                None,
-                {
-                    "hierarchy_name": proj.hierarchy_name,
-                    "description": proj.description,
-                    "lims_id": proj.lims_id,
-                },
-            )
-            tolqc.create(api_proj)
+    for file in file_list:
+        load_project_conf_file(mrshl, file)
+    mrshl.commit()
 
 
-def conf_file_Projects(conf_file):
-    for line in conf_file.open():
+def load_project_conf_file(mrshl, file):
+    for spec in conf_file_project_specs(file):
+        mrshl.update_or_create(Project, spec, ("lims_id",))
+
+
+def conf_file_project_specs(conf_file):
+    for line in pathlib.Path(conf_file).open():
         if re.match(r"^\s*#", line):
             continue
         if not re.search(r"\w", line):
             continue
         study_id, hierarchy, description = line.rstrip("\r\n").split("\t")
         hierarchy = re.sub(r"%TXGROUP%", "{}", hierarchy)
-        yield Project(
-            hierarchy_name=hierarchy,
-            lims_id=int(study_id),
-            description=description,
-        )
-        # yield study_id, hierarchy, description
+        yield {
+            "hierarchy_name": hierarchy,
+            "lims_id": int(study_id),
+            "description": description,
+        }
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    main()
 
 # from main.schema import ProjectSchema
 # schema = ProjectSchema()
