dc165d721a65e46d3ce901e3fbe6bf59ed079b77
James Gilbert <jgrg@sanger.ac.uk>
2023-09-27 15:35:30 +0100

    DuckDB report queries

--- src/tola/duckdb_pacbio_report.py
+++ src/tola/duckdb_pacbio_report.py
@@ -3,6 +3,7 @@ import duckdb
 import inspect
 import pathlib
 import sys
+import textwrap
 
 from functools import cache
 
@@ -10,7 +11,6 @@ FILE_TYPE = click.Path(
     dir_okay=False,
     exists=True,
     readable=True,
-    path_type=pathlib.Path,
 )
 
 
@@ -36,12 +36,130 @@ FILE_TYPE = click.Path(
 )
 @click.pass_context
 def cli(ctx, csv_files, duckdb_file):
-    if duckdb_file.exists():
-        ctx.fail(f"DuckDB database file '{duckdb_file}' already exists")
+    db_exists = duckdb_file.exists()
     con = duckdb.connect(str(duckdb_file))
+
+    if db_exists:
+        click.echo(f"DuckDB database file '{duckdb_file}' already exists", err=True)
+    else:
+        click.echo(
+            f"Creating and populating DuckDB database '{duckdb_file}' from CSV files {csv_files}",
+            err=True,
+        )
+        try:
+            create_and_populate_database(con, duckdb_file, csv_files)
+        except Exception as e:
+            duckdb_file.unlink(missing_ok=True)
+            msg = "Error creating DuckDB database"
+            raise Exception(msg) from e
+
+    table_row_counts(con)
+    full_report_grouped_by_idx(con, "pacbio_partitioned.csv")
+    report_multi_specimen_idx(con, "pacbio_multi_samples.csv")
+
+
+def create_and_populate_database(con, ducbdk_file, csv_files):
+    json_csv, rprt_csv = csv_files
+
+    con.begin()
+
+    # Create tables
     tables = "j", "r", "u"
     for name in tables:
         make_report_table(con, name)
+
+    # Populate j and r tables from CSV files
+    con.execute("INSERT INTO j SELECT * FROM read_csv_auto(?)", (json_csv,))
+    con.execute("INSERT INTO r SELECT * FROM read_csv_auto(?)", (rprt_csv,))
+
+    # Copy data from j and r tables into u
+    con.execute("INSERT INTO u SELECT * FROM j")
+    con.execute("INSERT INTO u SELECT * FROM r")
+
+    con.commit()
+
+
+def full_report_grouped_by_idx(con, csv_file):
+    """
+    Full report grouped by idx
+
+    idx is the movie name plus "#tag_index" (if any).
+    """
+
+    sql = """
+        SELECT idx
+          , ROW_NUMBER() OVER same_idx AS dup
+          , COUNT(*) OVER same_idx AS n_dup
+          , source
+          , project
+          , specimen
+          , * EXCLUDE(idx, source, project, specimen)
+        FROM u
+        WINDOW same_idx AS (
+          PARTITION BY idx
+          ORDER BY source, project, specimen
+          ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )
+    """
+    sql_to_csv(con, sql, csv_file)
+
+
+def report_multi_specimen_idx(con, csv_file):
+    """
+    Report where there is more than one specimen per idx
+
+    Counting the number of specimens within a WINDOW is a little awkward. The
+    DENSE_RANK function counts the number of unique entries seen in a column.
+    By using two WINDOWS, one ascending and the other descending they can be
+    added togther to get the number of unique values in the column.
+    """
+
+    sql = """
+        WITH dups AS (
+          SELECT idx
+          , ROW_NUMBER() OVER same_idx AS dup
+          , COUNT(*) OVER same_idx AS n_dup
+          , (DENSE_RANK() OVER asc_spcmn) + (DENSE_RANK() OVER desc_spcmn) - 1 AS n_spcmn
+          , source
+          , project
+          , specimen
+          , * EXCLUDE(idx, source, project, specimen)
+          FROM u
+          WINDOW same_idx AS (
+            PARTITION BY idx
+            ORDER BY source, project, specimen
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+          )
+          , asc_spcmn AS (
+            PARTITION BY idx
+            ORDER BY specimen ASC NULLS FIRST
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+          )
+          , desc_spcmn AS (
+            PARTITION BY idx
+            ORDER BY specimen DESC NULLS LAST
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+          )
+        )
+        SELECT * EXCLUDE(n_spcmn) FROM dups
+        WHERE n_spcmn > 1
+        ORDER BY idx, dup
+    """
+    sql_to_csv(con, sql, csv_file)
+
+
+def sql_to_csv(con, raw_sql, csv_file):
+    sql = inspect.cleandoc(raw_sql)
+    con.sql(sql).write_csv(csv_file, header=True)
+    info_sql = textwrap.indent(sql, "  ")
+    click.echo(f"\nWrote query:\n{info_sql}\nto file: '{csv_file}'")
+
+
+def table_row_counts(con):
+    for row in con.sql("SHOW TABLES").fetchall():
+        (tbl,) = row
+        (count,) = con.sql(f"SELECT COUNT(*) FROM '{tbl}'").fetchone()
+        click.echo(f"Table '{tbl}' has {count:6d} rows", err=True)
 
 
 def make_report_table(con, name):
