2401776e674ed92de65327c05ecefb5d4c866666
James Gilbert <jgrg@sanger.ac.uk>
2024-02-13 14:32:33 +0000

    Update ToL track projects

--- src/tola/projects_from_tol_track_conf.py
+++ src/tola/projects_from_tol_track_conf.py
@@ -7,7 +7,7 @@ import re
 import pathlib
 import tola.marshals
 
-from tolqc import Project
+from tolqc.sample_data_models import Project
 
 
 @click.command(help="Update list of Projects from tol_track.conf (or similar) file")
