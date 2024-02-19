848450569976677f88e732745d6ab2238a413e4e
James Gilbert <jgrg@sanger.ac.uk>
2024-02-19 12:02:31 +0000

    New duckdb version, and get TZ for ndjson encoder

--- src/tola/ndjson.py
+++ src/tola/ndjson.py
@@ -1,14 +1,15 @@
 import datetime
 import json
+import os
 import pytz
 import sys
 
 # Ideally the timezone could be collected from the source, such as the MLWH
 # MySQL server.
-DEFAULT_TZ = pytz.timezone("Europe/London")
+DEFAULT_TZ = pytz.timezone(os.getenv("TZ", "Europe/London"))
 
 
-class DateTimeEncoder(json.JSONEncoder):
+class DateTimeZoneEncoder(json.JSONEncoder):
     """
     Encode `date` and `datetime` objects in ISO 8601 format, and add the
     default, local timezone to any "naive" datetime objects before encoding.
@@ -25,6 +26,9 @@ class DateTimeEncoder(json.JSONEncoder):
         if isinstance(obj, datetime.date):
             return obj.isoformat()
 
+        # This line means any exceptions raised will come from the base class
+        return json.JSONEncoder.default(self, obj)
+
 
 def ndjson_row(data):
-    return json.dumps(data, cls=DateTimeEncoder, separators=(",", ":")) + "\n"
+    return json.dumps(data, cls=DateTimeZoneEncoder, separators=(",", ":")) + "\n"
