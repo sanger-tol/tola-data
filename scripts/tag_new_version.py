9504bf934196a45a6390ab800cb49a2f4b76b7e9
James Gilbert <jgrg@sanger.ac.uk>
2024-07-19 12:49:36 +0100

    Script for tagging with pyproject.toml version

---
+++ scripts/tag_new_version.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import tomllib
+
+from subprocess import CalledProcessError, run
+
+
+def main():
+    pyproj = "pyproject.toml"
+    with open(pyproj, "rb") as f:
+        data = tomllib.load(f)
+    version = data["project"]["version"]
+    gtv = git_tag_version()
+    if gtv:
+        if gtv == version:
+            exit(f"Tag for version {version} already exists")
+        elif natural(version) < natural(gtv):
+            exit(f"Version {version} in {pyproj} is less than latest git tag {gtv}")
+    run(("git", "tag", version), check=True)
+    run(("git", "push", "--tags"), check=True)
+
+
+def git_tag_version():
+    run(("git", "pull", "--tags"), check=True)  # noqa: S603
+    git_tag = run(("git", "tag"), capture_output=True, text=True, check=True)  # noqa: S603, S607
+    tags = sorted(git_tag.stdout.split("\n"), key=natural, reverse=True)
+    return tags[0] if tags else None
+
+
+def natural(string):
+    return tuple(
+        int(x) if i % 2 else x for i, x in enumerate(re.split(r"(\d+)", string))
+    )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except CalledProcessError as cpe:
+        exit(cpe.stderr)
