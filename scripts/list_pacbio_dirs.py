b949ee3ef9c3445defa17ac8bc8a6df3ca1a8dea
James Gilbert <jgrg@sanger.ac.uk>
2024-09-09 15:57:50 +0100

    Switch from data_id to PacBio run_id

--- scripts/list_pacbio_dirs.py
+++ scripts/list_pacbio_dirs.py
@@ -8,18 +8,18 @@ from tola.ndjson import ndjson_row
 conn = duckdb.connect(os.getenv("DIFF_MLWH_DUCKDB"))
 conn.execute(
     """
-    SELECT {
-      'data.id': data_id,
-      'specimen': tol_specimen_id,
-      'directory': concat('/lustre/scratch123/tol/tolqc'
+    SELECT DISTINCT {
+      'pacbio_run_metrics.id': run_id,
+      'directory': CONCAT(
+          '/lustre/scratch123/tol/tolqc'
         , parse_dirpath(remote_path[7:])
         , '/reports'
       )
     }
     FROM tolqc
-    WHERE starts_with(remote_path, 'irods:/seq/pacbio/')
-      AND tol_specimen_id IS NOT NULL
-    ORDER BY ALL
+    WHERE platform_type = 'PacBio'
+      AND starts_with(remote_path, 'irods:')
+    ORDER BY ALL;
     """
 )
 
