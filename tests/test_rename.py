c272da962435443c99c69f069bd0f5bca50bfaad
James Gilbert <jgrg@sanger.ac.uk>
2025-02-18 18:17:53 +0000

    Some generalisation of `tqc rename` code and start of specimen renaming

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -2,7 +2,7 @@ import pytest
 
 from tola.tqc.rename import (
     ToLQCRenameError,
-    build_species_rename_spec,
+    build_spec_dict,
     get_rename_field,
 )
 
@@ -25,41 +25,43 @@ def test_get_rename_field():
         assert get_rename_field(("a",), {"a": ["new", "new"]})
 
 
-def test_build_specie_rename_spec():
-    assert build_species_rename_spec(
+def test_build_species_rename_spec():
+    assert build_spec_dict(
+        "species",
         [
             {
-                "species.id": ["A a", "B b"],
-                "taxon_id": [3, 4],
+                "species.id": ["B b", "A a"],
+                "taxon_id": [4, 3],
             },
             {
-                "scientific_name": ["C c", "D d"],
-                "taxon_id": [5, 6],
+                "scientific_name": ["D d", "C c"],
+                "taxon_id": [6, 5],
             },
             {
-                "species_id": ["E e", "F f"],
+                "species_id": ["F f", "E e"],
             },
             {  # This duplicate spec is OK
-                "species.id": ["A a", "B b"],
-                "taxon_id": [3, 4],
+                "species.id": ["B b", "A a"],
+                "taxon_id": [4, 3],
             },
-        ]
+        ],
     ) == {
-        "A a": {"species.id": ["A a", "B b"], "taxon_id": [3, 4]},
-        "C c": {"species.id": ["C c", "D d"], "taxon_id": [5, 6]},
-        "E e": {"species.id": ["E e", "F f"], "taxon_id": None},
+        "A a": {"species.id": ["B b", "A a"], "taxon_id": [4, 3]},
+        "C c": {"species.id": ["D d", "C c"], "taxon_id": [6, 5]},
+        "E e": {"species.id": ["F f", "E e"], "taxon_id": None},
     }
 
-    with pytest.raises(ToLQCRenameError, match=r"Found .+ when storing"):
-        build_species_rename_spec(
+    with pytest.raises(ToLQCRenameError, match=r"Found .+ when building"):
+        build_spec_dict(
+            "species",
             [
                 {
-                    "species.id": ["A a", "B b"],
-                    "taxon_id": [3, 4],
+                    "species.id": ["B b", "A a"],
+                    "taxon_id": [4, 3],
                 },
                 {
-                    "scientific_name": ["A a", "D d"],
-                    "taxon_id": [5, 6],
+                    "scientific_name": ["D d", "A a"],
+                    "taxon_id": [6, 5],
                 },
-            ]
+            ],
         )
