fffada67e04e3380f1efafe149b1b97d82175508
James Gilbert <jgrg@sanger.ac.uk>
2025-02-19 14:40:43 +0000

    Refactor of `tqc rename` to allow renaming multiple tables

--- src/tola/tqc/rename.py
+++ src/tola/tqc/rename.py
@@ -17,6 +17,7 @@ class ToLQCRenameError(Exception):
 @click.pass_context
 @click.option(
     "--table",
+    "table_names",
     help="Name of table to rename",
     type=click.Choice(
         (
@@ -25,9 +26,10 @@ class ToLQCRenameError(Exception):
         ),
         case_sensitive=False,
     ),
+    multiple=True,
 )
 @click_options.input_files
-def rename(ctx, table, input_files):
+def rename(ctx, table_names, input_files):
     """
     Rename ToLQC database entries
 
@@ -41,72 +43,68 @@ def rename(ctx, table, input_files):
 
     client = ctx.obj
     input_obj = input_objects_or_exit(ctx, input_files)
+
+    for table in table_names:
+        try:
+            new_count, old_count = rename_records(client, table, input_obj)
+        except ToLQCRenameError as tre:
+            err = "Error: " + bold("\n".join(tre.args))
+            sys.exit(err)
+        if sys.stdout.isatty():
+            click.echo(
+                f"Created {bold(new_count)} new {table} and deleted {bold(old_count)}",
+                err=True,
+            )
+
+
+def rename_records(client, table, input_obj):
+    # Build a dictionary of renaming operations
     spec_dict = build_spec_dict(table, input_obj)
 
-    try:
-        if table == "species":
-            rename_species(client, table, spec_dict)
-        elif table == "specimen":
-            rename_specimens(client, table, spec_dict)
-    except ToLQCRenameError as tre:
-        err = "Error: " + bold("\n".join(tre.args))
-        sys.exit(err)
+    # Fetch all the old and existing "new" records
+    records_by_id = fetch_entries_from_specs(client, table, spec_dict)
 
+    if table == "species":
+        to_many_tables = ("specimen", "umbrella")
+        # If there's no taxon_id in the new species, fetch it from the old
+        fill_mising_taxon_id_from_old_species(spec_dict, records_by_id)
 
-def rename_species(client, table, spec_dict):
-    # Fetch all of the old and existing "new" species
-    species_by_id = fetch_entries_from_specs(client, table, spec_dict)
+        # Create new species objects from GoaT via taxon_id (and check scientific
+        # name matches the expected new species.id).
+        new_records = create_new_species_from_goat(client, spec_dict, records_by_id)
 
-    # If there's no taxon_id in the new species, fetch it from the old
-    fill_mising_taxon_id_from_old_species(spec_dict, species_by_id)
+        # Avoid collisions in tolid_prefix
+        rename_tolid_prefix_in_old_species(client, spec_dict, records_by_id)
+    elif table == "specimen":
+        to_many_tables = ("sample", "specimen_status")
 
-    # Create new species objects from GoaT via taxon_id (and check scientific
-    # name matches the expected new species.id).
-    new_species = create_new_species_from_goat(client, spec_dict, species_by_id)
+        ### Will need code here to handle the offspring table once it is populated
 
-    # Avoid collisions in tolid_prefix
-    rename_tolid_prefix_in_old_species(client, spec_dict, species_by_id)
+        # Create any new specimens required, copying any field values from the
+        # old entries
+        new_records = create_missing_new_objs_from_old(
+            client, table, records_by_id, spec_dict
+        )
 
-    # Store new species entries
-    client.ads.upsert(table, new_species)
+    # Store new records
+    client.ads.upsert(table, new_records)
 
-    # In specimens, change to new species_id
-    switch_to_many_entries(client, spec_dict, table, "specimen")
+    # Switch to-many related objects to point to new records
+    for to_many in to_many_tables:
+        switch_to_many_entries(client, spec_dict, table, to_many)
 
-    # Delete any edit_species entries linked to old species
-    delete_old_edit_species(client, spec_dict)
+    # Delete old edit_... table entries
+    delete_old_edit_entries(client, table, spec_dict)
 
-    # Delete old species entries
+    # Delete old records
     client.ads.delete(table, list(spec_dict))
 
-    if sys.stdout.isatty():
-        new = bold(len(new_species))
-        old = bold(len(spec_dict))
-        click.echo(f"Created {new} new species and deleted {old}", err=True)
-
-
-def rename_specimens(client, table, spec_dict):
-    # Fetch old and any existing "new" specimens
-    spcmns_by_id = fetch_entries_from_specs(client, table, spec_dict)
-
-    # Create any new specimens required, copying all the fields from the old
-    # entries
-    new_spcmns = create_missing_new_objs_from_old(
-        client, table, spcmns_by_id, spec_dict
-    )
-
-    # Store the new specimens
-    client.ads.upsert(table, new_spcmns)
-
-    # Switch samples to new specimens
-    switch_to_many_entries(client, spec_dict, table, "sample")
-
-    ### Will need code here to handle the offspring table once it is populated
+    return len(new_records), len(spec_dict)
 
 
 def create_missing_new_objs_from_old(client, table, spcmns_by_id, spec_dict):
-    ads = client.ads
-    obj_bldr = ads.data_object_factory
+    cdo = client.build_cdo
+
     new_obj = []
     pk_name = f"{table}.id"
     for spec in spec_dict.values():
@@ -120,11 +118,11 @@ def create_missing_new_objs_from_old(client, table, spcmns_by_id, spec_dict):
             if k == pk_name:
                 continue
             new_key = k.replace(".id", "_id")
-            # There are some acceptions to the naming of foreign keys
+            # There are some exceptions to the naming of foreign keys
             if new_key == "status_id":
                 new_key = f"{table}_{new_key}"
             new_attr[new_key] = v
-        new_obj.append(obj_bldr(table, id_=new, attributes=new_attr))
+        new_obj.append(cdo(table, new, new_attr))
 
     return new_obj
 
@@ -165,37 +163,38 @@ def fill_mising_taxon_id_from_old_species(spec_dict, species_by_id):
         spec["taxon_id"] = [old_txn, old_txn]
 
 
-def delete_old_edit_species(client, spec_dict):
+def delete_old_edit_entries(client, table, spec_dict):
     ads = client.ads
+    edit_table = f"edit_{table}"
     old_edits = ads.get_list(
-        "edit_species",
-        object_filters=DataSourceFilter(in_list={"species_id": list(spec_dict)}),
+        edit_table,
+        object_filters=DataSourceFilter(in_list={f"{table}_id": list(spec_dict)}),
     )
-    ads.delete("edit_species", [x.id for x in old_edits])
+    ads.delete(edit_table, [x.id for x in old_edits])
 
 
 def rename_tolid_prefix_in_old_species(client, spec_dict, species_by_id):
-    ads = client.ads
-    obj_bldr = ads.data_object_factory
+    cdo = client.build_cdo
+
     old_species = [species_by_id[x] for x in spec_dict]
     rename_taxon_id = [
-        obj_bldr(
-            "species",
-            id_=x["species.id"],
-            attributes={"tolid_prefix": x["tolid_prefix"] + "-deltmp"},
-        )
+        cdo("species", x["species.id"], {"tolid_prefix": x["tolid_prefix"] + "-deltmp"})
         for x in old_species
     ]
-    for sp in ads.upsert("species", rename_taxon_id):
+    for sp in client.ads.upsert("species", rename_taxon_id):
         species_by_id[sp.id] = core_data_object_to_dict(sp)
 
 
 def switch_to_many_entries(client, spec_dict, table, many_tbl):
+    """
+    Switch the foreign key in each `to_many` related object to point to its
+    new (renamed) parent.
+    """
     ads = client.ads
-    obj_bldr = ads.data_object_factory
+    cdo = client.build_cdo
 
-    pk_name = f"{table}.id"
     fk_name = f"{table}_id"
+    pk_name = f"{table}.id"
     many_pk_name = f"{many_tbl}.id"
 
     many = [
@@ -205,17 +204,20 @@ def switch_to_many_entries(client, spec_dict, table, many_tbl):
             object_filters=DataSourceFilter(in_list={fk_name: list(spec_dict)}),
         )
     ]
-    old_new = {x: y[pk_name][0] for x, y in spec_dict.items()}
-    switch_obj = [
-        obj_bldr(many_tbl, id_=x[many_pk_name], attributes={fk_name: old_new[x[pk_name]]})
-        for x in many
-    ]
+
+    switch_obj = []
+    for m in many:
+        old_fk = m[pk_name]
+        new_fk = spec_dict[old_fk][pk_name][0]
+        name = m[many_pk_name]
+        switch_obj.append(cdo(many_tbl, name, {fk_name: new_fk}))
+
     ads.upsert(table, switch_obj)
 
 
 def create_new_species_from_goat(client, spec_dict, species_by_id):
-    obj_factory = client.ads.data_object_factory
     gc = GoaTClient()
+    cdo = client.build_cdo
 
     err = ""
     new_species = []
@@ -232,7 +234,7 @@ def create_new_species_from_goat(client, spec_dict, species_by_id):
                 f" is named {goat_species!r} in GoaT"
             )
             continue
-        new_species.append(obj_factory("species", id_=new_id, attributes=info))
+        new_species.append(cdo("species", new_id, info))
     if err:
         raise ToLQCRenameError(err)
 
@@ -267,7 +269,9 @@ def build_spec_dict(table, input_obj):
 
     renames = {}
     for obj in input_obj:
-        rename_id = get_rename_field(fld_names, obj)
+        rename_id = get_rename_field(fld_names, obj, maybe=True)
+        if not rename_id:
+            continue
         spec = {pk: rename_id}
         for fld, maybe in other_flds:
             if isinstance(fld, tuple):
