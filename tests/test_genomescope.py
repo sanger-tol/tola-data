24c561ac4d4b98bbb6a332997d82ab267c536cd7
James Gilbert <jgrg@sanger.ac.uk>
2026-01-19 18:39:44 +0000

    Updated genomescope tests for refactoring

--- tests/test_genomescope.py
+++ tests/test_genomescope.py
@@ -10,15 +10,37 @@ from tola.tqc.dataset import find_dataset_file, latest_dataset
 from tola.tqc.genomescope import (
     attr_from_report,
     build_genomescope_cmd_line,
-    get_genomescope_params,
+    fetch_specimen_ploidy,
+    file_json_contents,
+    find_report_file,
+    genomescope_params_from_previous_run,
     parse_summary_txt,
-    report_json_contents,
 )
 from tola.tqc.tqc_cmd import tqc_main
 
 
 def gs_results_dir(base):
     return base / "pacbio" / "kmer" / "k31"
+
+
+@pytest.fixture
+def dataset(client, fofn_dir):
+    return get_or_load_dataset(client, fofn_dir)
+
+
+def get_or_load_dataset(client, here):
+    ds_file = find_dataset_file(here)
+    if not ds_file:
+        msg = f"Failed to find dataset file in {here = }"
+        raise ValueError(msg)
+    dataset = latest_dataset(ds_file)
+
+    # Ensure dataset is loaded
+    rspns = client.ndjson_post("loader/dataset", [ndjson_row(dataset)])
+    dataset = rspns.get("new", rspns.get("existing"))[0]
+    ds_file.write_text(ndjson_row(dataset))
+
+    return dataset
 
 
 @pytest.fixture(scope="session")
@@ -28,20 +50,24 @@ def gscope_fldr_loc(ads):
         "files_template": {
             "image_file_patterns": [
                 {
-                    "caption": "Genomescope 2.0 linear plot",
-                    "pattern": ".*linear_plot\\.png",
-                },
-                {
-                    "caption": "Genomescope 2.0 log plot",
-                    "pattern": ".*log_plot\\.png",
-                },
-                {
                     "caption": "Genomescope 2.0 transformed linear plot",
                     "pattern": ".*transformed_linear_plot\\.png",
+                    "index": 2,
+                },
+                {
+                    "caption": "Genomescope 2.0 linear plot",
+                    "pattern": ".*linear_plot\\.png",
+                    "index": 0,
                 },
                 {
                     "caption": "Genomescope 2.0 transformed log plot",
                     "pattern": ".*transformed_log_plot\\.png",
+                    "index": 3,
+                },
+                {
+                    "caption": "Genomescope 2.0 log plot",
+                    "pattern": ".*log_plot\\.png",
+                    "index": 1,
                 },
             ],
             "other_file_patterns": [
@@ -61,14 +87,20 @@ def gscope_fldr_loc(ads):
     return fldr_loc
 
 
+def test_fetch_specimen_ploidy(client, dataset):
+    ploidy = fetch_specimen_ploidy(client, dataset["dataset.id"])
+    assert ploidy == 6
+
+
 def test_get_params(fofn_dir):
     rdir = gs_results_dir(fofn_dir)
-    params = get_genomescope_params(rdir)
+    params = genomescope_params_from_previous_run(rdir)
     assert params == {
-        "--input": "tests/hugegenome.hist",
+        "--input": "fastk.hist.txt",
         "--kmer_length": "31",
-        "--output": "test_out",
-        "--ploidy": "2",
+        "--name_prefix": "fastk_genomescope",
+        "--output": ".",
+        "--ploidy": "6",
     }
 
 
@@ -76,17 +108,19 @@ def test_build_genomescope_cmd_line(fofn_runner):  # noqa: ARG001
     rdir = gs_results_dir(Path("./fofn"))
     files = list(rdir.iterdir())
     logging.info(files)
-    cmd_line = build_genomescope_cmd_line(rdir)
+    cmd_line = build_genomescope_cmd_line({}, rdir)
     assert cmd_line == [
         "genomescope.R",
-        "--input",
-        "fofn/pacbio/kmer/k31/test.hist.txt",
-        "--output",
-        "fofn/pacbio/kmer/k31",
         "--ploidy",
         "2",
         "--kmer_length",
         "31",
+        "--name_prefix",
+        "fastk_genomescope",
+        "--input",
+        "fastk.hist.txt",
+        "--output",
+        ".",
         "--json_report",
     ]
 
@@ -124,41 +158,54 @@ def test_parse_summary_txt():
 
 
 def test_process_report(fofn_dir):
-    assert (rprt := report_json_contents(gs_results_dir(fofn_dir)))
+    assert (rprt_file := find_report_file(gs_results_dir(fofn_dir)))
+    rprt = file_json_contents(rprt_file)
     flat = attr_from_report(rprt)
     assert flat == {
         "kmer": 31,
-        "ploidy": 2,
+        "ploidy": 6,
         "kcov_init": None,
-        "homozygous": 0.998564047901143,
-        "heterozygous": 0.00143595209885725,
-        "haploid_length": 5878521033,
-        "unique_length": 3981236382,
-        "repeat_length": 1897284651,
-        "kcov": 6.56564770932383,
-        "model_fit": 0.992057997138315,
-        "read_error_rate": 0.00269857308904897,
+        "homozygous": 0.93060537607628,
+        "heterozygous": 0.0693946239237199,
+        "haploid_length": 1443711482,
+        "unique_length": 458240204,
+        "repeat_length": 985471278,
+        "kcov": 17.346385141911,
+        "model_fit": 0.919504443600502,
+        "read_error_rate": 0.00170901574325055,
         "results": rprt,
     }
 
 
-def test_load_genomescope(client, fofn_runner, test_alias, gscope_fldr_loc):
-    here = Path("./fofn")
-    ds_file = find_dataset_file(here)
-    dataset = latest_dataset(ds_file)
+def test_load_genomescope(dataset, fofn_runner, test_alias, gscope_fldr_loc):
+    rdir = gs_results_dir(Path("./fofn"))
+    args = (
+        "--tolqc-alias",
+        test_alias,
+        "genomescope",
+        "--upload",
+        "--folder-location",
+        gscope_fldr_loc.id,
+        str(rdir),
+    )
+    result = fofn_runner.invoke(tqc_main, args)
+    assert result.exit_code == 0
+    out = json.loads(result.stdout)
+    logging.info("\n" + json.dumps(out, indent=2))
+    assert dataset["dataset.id"] == out["dataset.id"]
+    assert len(out["image_file_list"]) == 4
+    assert len(out["other_file_list"]) == 1
 
-    # Ensure dataset is loaded
-    rspns = client.ndjson_post("loader/dataset", [ndjson_row(dataset)])
-    dataset = rspns.get("new", rspns.get("existing"))[0]
-    ds_file.write_text(ndjson_row(dataset))
 
+def test_run_genomescope(dataset, fofn_runner, test_alias, gscope_fldr_loc):
+    rdir = gs_results_dir(Path("./fofn"))
     args = (
         "--tolqc-alias",
         test_alias,
         "genomescope",
         "--folder-location",
         gscope_fldr_loc.id,
-        str(gs_results_dir(here)),
+        str(rdir),
     )
     result = fofn_runner.invoke(tqc_main, args)
     assert result.exit_code == 0
