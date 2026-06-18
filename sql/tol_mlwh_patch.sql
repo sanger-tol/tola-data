1178e46c65dd9fc0f0bac08e3d85531f34a4c424
James Gilbert <jgrg@sanger.ac.uk>
2026-06-18 17:29:44 +0100

    Set most columns to non-null

--- sql/tol_mlwh_patch.sql
+++ sql/tol_mlwh_patch.sql
@@ -7,43 +7,43 @@ COMMENT 'Internal primary key';
 ALTER TABLE tol_sample_bioproject CHANGE COLUMN id_sample_tmp id_sample_tmp int unsigned DEFAULT NULL
 COMMENT 'Sample ID, see `sample.id_sample_tmp`';
 
-ALTER TABLE tol_sample_bioproject CHANGE COLUMN `file` `file` varchar(500) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject CHANGE COLUMN `file` `file` varchar(500) NOT NULL
 COMMENT 'Path to the source file';
 
 ALTER TABLE tol_sample_bioproject CHANGE COLUMN filename filename varchar(255) DEFAULT NULL
 COMMENT 'Name which should be given to the submitted file' AFTER file;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN platform varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN platform varchar(40) NOT NULL
 COMMENT 'ENA PLATFORM metadata field. Not required if INSTRUMENT is provided' AFTER filename;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN instrument varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN instrument varchar(40) NOT NULL
 COMMENT 'ENA INSTRUMENT metadata field' AFTER platform;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN library_name varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN library_name varchar(40) NOT NULL
 COMMENT 'ENA LIBRARY_NAME metadata field' AFTER instrument;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN library_source varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN library_source varchar(40) NOT NULL
 COMMENT 'ENA LIBRARY_SOURCE metadata field' AFTER library_name;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN library_selection varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN library_selection varchar(40) NOT NULL
 COMMENT 'ENA LIBRARY_SELECTION metadata field' AFTER library_source;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN library_strategy varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN library_strategy varchar(40) NOT NULL
 COMMENT 'ENA LIBRARY_STRATEGY metadata field' AFTER library_selection;
 
-ALTER TABLE tol_sample_bioproject CHANGE COLUMN library_type library_type varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject CHANGE COLUMN library_type library_type varchar(40) NOT NULL
 COMMENT 'Temporary home for ENA LIBRARY_CONSTRUCTION_PROTOCOL metadata field during migration to library_construction_protocol';
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN library_construction_protocol varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN library_construction_protocol varchar(40) NOT NULL
 COMMENT 'ENA LIBRARY_CONSTRUCTION_PROTOCOL metadata field' AFTER library_type;
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN design_description varchar(500) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN design_description varchar(500) NOT NULL
 COMMENT 'ENA DESCRIPTION metadata field. Free text description of the library' AFTER library_construction_protocol;
 
-ALTER TABLE tol_sample_bioproject CHANGE COLUMN tolid tolid varchar(40) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject CHANGE COLUMN tolid tolid varchar(40) NOT NULL
 COMMENT 'Tree of Life ID, see: https://id.tol.sanger.ac.uk';
 
-ALTER TABLE tol_sample_bioproject ADD COLUMN data_id varchar(128) DEFAULT NULL
+ALTER TABLE tol_sample_bioproject ADD COLUMN data_id varchar(128) NOT NULL
 COMMENT 'Value from ToLQC database `data.data_id`' AFTER tolid;
 
 ALTER TABLE tol_sample_bioproject CHANGE COLUMN biosample_accession biosample_accession varchar(255) DEFAULT NULL
@@ -57,5 +57,11 @@ COMMENT 'When row was created';
 
 ALTER TABLE tol_sample_bioproject CHANGE COLUMN date_updated date_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 COMMENT 'When row was last updated';
+
+-- ALTER TABLE tol_sample_bioproject ADD UNIQUE (data_id);
+
+UPDATE tol_sample_bioproject
+SET library_construction_protocol = library_type
+WHERE library_construction_protocol IS NULL;
 
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
